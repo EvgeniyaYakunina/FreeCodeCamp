7f20b0209f629d5b25ab095d78f4874e99dc2eaa
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-10 16:35:36 +0300

    add tasks 3, 4

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -9,3 +9,31 @@ function reusableFunction(){
     console.log(a + b)
   }
   functionWithArgs(1, 2);
+
+  // Return a Value from a Function with Return
+  function timesFive(num) {
+    return num * 5;
+  }
+  const answer = timesFive(5);
+
+  // Global Scope and Functions
+
+  // Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+oopsGlobal = 5;
+}
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
