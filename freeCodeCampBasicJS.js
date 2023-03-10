197594a521a56d09cbe6291650bd95d25793542a
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-10 13:51:34 +0300

    add 2 task

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -1,4 +1,11 @@
+// Write Reusable JavaScript with Functions
 function reusableFunction(){
     console.log("Hi World");
   } 
   reusableFunction();
+
+  // Passing Values to Functions with Arguments
+  function functionWithArgs(a, b){
+    console.log(a + b)
+  }
+  functionWithArgs(1, 2);
