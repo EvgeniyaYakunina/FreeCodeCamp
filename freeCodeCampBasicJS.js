8114c41fe1480caeab993319e22b3a7f095b510c
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-11 12:16:33 +0300

    add task 5

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -3,29 +3,31 @@ function reusableFunction(){
     console.log("Hi World");
   } 
   reusableFunction();
+// **********************************************************************************************
+
 
   // Passing Values to Functions with Arguments
   function functionWithArgs(a, b){
     console.log(a + b)
   }
   functionWithArgs(1, 2);
+  // **********************************************************************************************
+
 
   // Return a Value from a Function with Return
   function timesFive(num) {
     return num * 5;
   }
   const answer = timesFive(5);
+  // **********************************************************************************************
+
 
   // Global Scope and Functions
-
-  // Declare the myGlobal variable below this line
 const myGlobal = 10;
 
 function fun1() {
-  // Assign 5 to oopsGlobal here
 oopsGlobal = 5;
 }
-// Only change code above this line
 
 function fun2() {
   let output = "";
@@ -37,3 +39,15 @@ function fun2() {
   }
   console.log(output);
 }
+// **********************************************************************************************
+
+// Local Scope and Functions
+function myLocalScope() {
+  
+const myVar = 3;
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+console.log('inside myLocalScope', myVar)
+
+console.log('outside myLocalScope', myVar);
