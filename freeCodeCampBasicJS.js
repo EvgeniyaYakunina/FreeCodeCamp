06bd93c21a955dd4f6ad0b80982e28323b6c9ee6
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-13 20:12:41 +0300

    add tasks 10-13

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -104,3 +104,47 @@ console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 // **********************************************************************************************
+
+// 10. Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+  
+if(wasThatTrue){
+  return "Yes, that was true";
+}
+return "No, that was false";
+
+}
+// **********************************************************************************************
+
+// 11. Comparison with the Equality Operator
+function testEqual(val) {
+  if (val == 12) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+// **********************************************************************************************
+
+// 12. Comparison with the Strict Equality Operator
+function testStrict(val) {
+  if (val === 7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+// **********************************************************************************************
+
+// 13. Practice comparing different values
+function compareEquality(a, b) {
+  if (a === b) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+// **********************************************************************************************
