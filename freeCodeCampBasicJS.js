839471839b50a36a7cf78ee8302de553fcbc6e14
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-15 09:16:29 +0300

    add tasks 14-21

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -148,3 +148,114 @@ function compareEquality(a, b) {
 
 compareEquality(10, "10");
 // **********************************************************************************************
+
+// 14. Comparison with the Inequality Operator
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testNotEqual(10);
+// **********************************************************************************************
+
+//  15. Comparison with the Strict Inequality Operator
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+// **********************************************************************************************
+
+// 16. Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+
+  if (val > 10) { 
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+// **********************************************************************************************
+
+// 17. Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+// **********************************************************************************************
+
+// 18. Comparison with the Less Than Operator
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+// **********************************************************************************************
+
+// 19. Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+// **********************************************************************************************
+
+// 20. Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+
+  if (val <= 50 && val >= 25) { 
+      return "Yes"; 
+  }
+  return "No";
+}
+
+testLogicalAnd(10);
+// **********************************************************************************************
+
+// 21. Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+
+testLogicalOr(15);
+// **********************************************************************************************
+
+
