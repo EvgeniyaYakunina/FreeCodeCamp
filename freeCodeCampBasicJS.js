8ad60a7b4e1fec3e6041218dea726d6893392c32
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-16 10:39:36 +0300

    add tasks 22-25

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -258,4 +258,64 @@ function testLogicalOr(val) {
 testLogicalOr(15);
 // **********************************************************************************************
 
+// 22. Introducing Else Statements
+function testElse(val) {
+  let result = "";
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }else{
+    return "5 or Smaller"
+  }
+  return result;
+}
+
+testElse(4);
+// **********************************************************************************************
+
+// 23. Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+}
+}
+testElseIf(7);
+// **********************************************************************************************
+// 24. Logical Order in If Else Statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+// **********************************************************************************************
+
+// 25. Chaining If Else Statements
+function testSize(num) {
+ 
+  if(num < 5){
+    return "Tiny"
+  } else if(num < 10){
+    return "Small"
+  }else if(num < 15){
+    return "Medium"
+  }else if(num < 20){
+    return "Large"
+  }else{
+    return "Huge";
+  }
+}
+testSize(7);
+
+
+
 
