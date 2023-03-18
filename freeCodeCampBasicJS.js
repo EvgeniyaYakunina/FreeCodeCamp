0ab4a5b5c47e47df78f8b56cfcac6a9fdf1f1288
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-18 11:47:29 +0300

    add tasks 29-32

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -389,3 +389,78 @@ switch (val){
 switchOfStuff(1);
 // **********************************************************************************************
 
+// 29. Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+ 
+switch(val){
+  case 1: 
+  case 2: 
+  case 3: 
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break; 
+}
+  return answer;
+}
+sequentialSizes(1);
+// **********************************************************************************************
+
+// 30. Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+}
+  return answer;
+
+
+chainToSwitch(7);
+}
+// **********************************************************************************************
+
+// 31. Returning Boolean Values from Functions
+function isLess(a, b) {
+
+  return a < b;
+}
+
+isLess(10, 15);
+// **********************************************************************************************
+
+// 32. Return Early Pattern for Functions
+function abTest(a, b) {
+ 
+if (a < 0 || b < 0) {
+
+return underfined;
+}
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+// **********************************************************************************************
