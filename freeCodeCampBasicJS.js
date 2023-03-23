6eb6a70dd63e982b1256fc963d1172e3ddef99b0
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-23 18:34:49 +0300

    add tasks 54-58

--- freeCodeCampBasicJS.js
+++ freeCodeCampBasicJS.js
@@ -791,3 +791,78 @@ do{
   j++;
 } while (j < 11); 
 // **********************************************************************************************
+
+// 54. Replace Loops using Recursion
+function sum(arr, n) {
+
+if(n <= 0){
+  return 0;
+}else{
+  return sum(arr, n - 1) + arr[n - 1];
+}
+}
+// **********************************************************************************************
+
+// 55. Profile Lookup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+for(let i = 0; i < contacts.length; i++){
+  if(name === contacts[i].firstName){
+    return contacts[i][prop] || "No such property";
+  }
+}
+return "No such contact";
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
+// **********************************************************************************************
+
+// 56. Generate Random Fractions with JavaScript
+function randomFraction() {
+
+  return Math.random();
+}
+// **********************************************************************************************
+
+// 57. Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+
+  return Math.floor(Math.random() * 10);
+}
+// **********************************************************************************************
+
+// 58. Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+}
+// **********************************************************************************************
